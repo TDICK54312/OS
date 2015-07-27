6c21ec0091f3abf97a260a36d19b7a3f88a3125e
Tim Dickson <TDICK@ImAGhost.local>
2015-07-26 23:56:30 -0500

    Have a bug with sem_getvalue()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<sys/shm.h>
+#include<sys/sem.h>
 #include<sys/types.h>
 #include<sstream>
 #include<unistd.h>
@@ -27,17 +29,44 @@ struct Car{
 }*/
 
 int main(int args, char** argv){
-    int i = 0;
-    list<Car> carsList;
-    int count = 0;
-    string stuff[4];
-    string line;
-    int pid = -1;
+    int i = 0;                          //used in parser
+    list<Car> carsList;                 //creates a list of cars to be accessed FCFS
+    int count = 0;                      //used to see how many carsare in the list
+    string stuff[4];                    //used in parser 
+    string line;                        //used for parser
+    int pid = -1;                       //initialize pid
+    int *MAX_WEIGHT; //= atoi(argv[1]); //shared variable
+    key_t shmkey;                       //shared memory key
+    int shmid;                          //shared memory id
+    int value;                          //semaphore value to check if 1 or 0
+    //size_t sizechar = NBYTES;           //size of shared memory
+    sem_t *mutex;                        //synch semaphore 
 
-    unsigned int MAX_WEIGHT = atoi(argv[1]);
+    /* initialize shared variable in shared memory */
+    shmid = shmget(shmkey, sizeof(int), 0600 | IPC_CREAT);
 
-    cout<<MAX_WEIGHT<<"\n";
+    /* shared memory error check */
+    if(shmid < 0){
+        perror("shmget\n");
+        exit(1);
+    }
 
+    /* makes sure there are two arguments */
+    if(args != 2){
+        perror("Wrong number of arguments.");
+        exit(1);
+    }
+    
+    /* Attatch MAX_WEIGHT to shared memory */
+    MAX_WEIGHT = (int *) shmat (shmid, NULL, 0);
+    *MAX_WEIGHT = atoi(argv[1]);
+    cout<<"MAX_WEIGHT on bridge "<<*MAX_WEIGHT<< " is allocated in shared memory."<<endl;
+
+
+    //Used to make sure i was getting the max weight
+    //cout<<MAX_WEIGHT<<"\n";
+    
+    /* This gets the input from input1.txt */
     while(getline(cin,line)){
         stringstream ssin(line);
         Car x;
@@ -55,26 +84,95 @@ int main(int args, char** argv){
             carsList.pop_front();
         }
 
+        if(x.carWeight > *MAX_WEIGHT)
+        {
+            perror("You entered an invalid weight. Please change text file.");
+            _exit(1);
+        }
+
         i = 0;
         count++;
     }
-    //Just making sure the list was getting all the values it need *works*
-    /*for(list<Car>::iterator it = carsList.begin(); it != carsList.end(); ++it){
-        Car m = *it;
-
-        cout<<"License Plate #: "<<m.plate<<" Enters at: "<<m.secondsArrival<<" Car weight: "<<m.carWeight<<" Leaving at: "<<m.secondsLeave<<endl;
-    }*/
     
+    /* Create Semaphore */
+    mutex = sem_open("TD", O_CREAT, 0600, 1);
+    /* Unlink prevents the semaphore from existing forever */
+    sem_unlink("TD");
+    
+    /* Forking process */
     for(list<Car>::iterator it = carsList.begin(); it != carsList.end(); ++it){
-        //Car newCar = *it;
-
+        
         pid = fork();
         if(pid == 0){
             Car newCar = *it;
-            cout<<"Waiting "<<newCar.secondsArrival<<" seconds."<<endl;
+
+            /* Print arrival of car */
             sleep(newCar.secondsArrival);
-            
-            
+            cout<<"Car: "<<newCar.plate<<" arrives."<<"     BridgeLoad Available: "<<*MAX_WEIGHT<<endl;
+            int s = sem_getvalue(*mutex, &value);
+            if(s == -1)
+            {
+                perror("error get value from semaphore.");
+                _exit(1);
+            }
+            if(value == 1){
+                if(*MAX_WEIGHT - newCar.carWeight >= 0){
+                    *MAX_WEIGHT = *MAX_WEIGHT - newCar.carWeight;
+                    cout<<"Car: "<<newCar.plate<<" enters bridge."<<"   BridgeLoad Available: "<<*MAX_WEIGHT<<endl;
+                    sleep(newCar.secondsLeave);
+                    *MAX_WEIGHT = *MAX_WEIGHT + newCar.carWeight;
+                    cout<<"Car: "<<newCar.plate<<" leaves bridge."<<"   BridgeLoade Available: "<<*MAX_WEIGHT<<endl;
+                }
+                if(*MAX_WEIGHT - newCar.carWeight < 0){
+                    sem_wait(mutex);
+                    bool x = true;
+                    while(x == true){
+                        if(*MAX_WEIGHT - newCar.carWeight >= 0){
+                            x = false;
+                        }
+                    }
+                    *MAX_WEIGHT = *MAX_WEIGHT - newCar.carWeight;
+                    cout<<"Car: "<<newCar.plate<<" enters bridge."<<"   BridgeLoad Available: "<<*MAX_WEIGHT<<endl;
+                    sleep(newCar.secondsLeave);
+                    *MAX_WEIGHT = *MAX_WEIGHT + newCar.carWeight;
+                    cout<<"Car: "<<newCar.plate<<" leaves bridge."<<"   BridgeLoade Available: "<<*MAX_WEIGHT<<endl;
+                    sem_post(mutex);
+                }
+            }
+            if(value == 0)
+            {
+                bool y = true;
+
+                while(y == true){
+                    if(*MAX_WEIGHT - newCar.carWeight >= 0){
+                        y = false;
+                    }
+                }
+                if(value == 1){
+                    if(*MAX_WEIGHT - newCar.carWeight >= 0){
+                        *MAX_WEIGHT = *MAX_WEIGHT - newCar.carWeight;
+                        cout<<"Car: "<<newCar.plate<<" enters bridge."<<"   BridgeLoad Available: "<<*MAX_WEIGHT<<endl;
+                        sleep(newCar.secondsLeave);
+                        *MAX_WEIGHT = *MAX_WEIGHT + newCar.carWeight;
+                        cout<<"Car: "<<newCar.plate<<" leaves bridge."<<"   BridgeLoade Available: "<<*MAX_WEIGHT<<endl;
+                    }
+                    if(*MAX_WEIGHT - newCar.carWeight < 0){
+                        sem_wait(mutex);
+                        bool a = true;
+                        while(a == true){
+                            if(*MAX_WEIGHT - newCar.carWeight >= 0){
+                                a = false;
+                            }
+                        }
+                        *MAX_WEIGHT = *MAX_WEIGHT - newCar.carWeight;
+                        cout<<"Car: "<<newCar.plate<<" enters bridge."<<"   BridgeLoad Available: "<<*MAX_WEIGHT<<endl;
+                        sleep(newCar.secondsLeave);
+                        *MAX_WEIGHT = *MAX_WEIGHT + newCar.carWeight;
+                        cout<<"Car: "<<newCar.plate<<" leaves bridge."<<"   BridgeLoade Available: "<<*MAX_WEIGHT<<endl;
+                        sem_post(mutex);
+                }
+              }
+            }
 
             break;
 
